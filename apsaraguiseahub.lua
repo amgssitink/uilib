310bec9e7aab0a2199ad137ee3b71ffd93da519f
phoriphaisitink <166979659+amgssitink@users.noreply.github.com>
2024-06-13 12:01:46 +0700

    Add files via upload

--- apsaraguiseahub.lua
+++ apsaraguiseahub.lua
@@ -1,23 +1,53 @@
 local SettingsAPSARA = {}
 local Home = win.CreatePage({Page_Name = "Home",Page_Title = "Home"})
-local Section = Home.CreateSection("Main")
-Section.CreateToggle({Title = "Toggle",Desc = "Test",Default = false},function(v)
-print(v)
+
+local Page1 = windowz:CreatePage("Page I")
+local Page2 = windowz:CreatePage("Page II")
+
+local Section1 = Page1:CreateSection("Section I")
+local Section2 = Page2:CreateSection("Section II")
+
+Section2:CreateKeybind("Keybind", "T", function(key)
+	print("Pressed "..tostring(key))
 end)
 
-Section.CreateButton({Title = "Button"},function()
-    print('Click')
-end)
-Section.CreateLabel({Title = "Label"})
-Section.CreateBox({Title = "TextBox",Placeholder = "Place holder"},function(v)
-    print(v)
-end)
-Section.CreateSlider({Title = "Slider",Min = 0,Max = 100,Precise = false,Default = 50},function(v)
-    print(v)
+Section1:CreateSlider("Slider Example", {Min = 16, Max = 500, DefaultValue = 30}, function(Value)
+	print(Value)
 end)
 
-Section.CreateDropdown({Title = "Dropdown",List = {'A','B'},Default = 'A',Precise = false,Search = false},function(v)
-    print(v)
+Section1:CreateToggle("Toggle Example", {Toggled = false , Description = "Iu Tvk nhìu UwU"}, function(Value)
+	print(Value)
+end)
+
+Section1:CreateButton("Button Example", function ()
+	print("Button Cliked!")
+end)
+
+Section1:CreateTextbox("TextBox", false, function (vv)
+	print(vv)
+end)
+
+Section1:CreateDropdown("Dropdown ", {
+	List = {"Value1", "Value2", "Value3", "Value4"},
+	Default = "None"}, function(value)
+	print(value)
+end)
+
+Section1:CreateColorPicker("Color Picker", Color3.fromRGB(255, 255, 255), function (uwu)
+	print(uwu)
+end)
+
+local dropdown = Section1:CreateDropdown("Refresh Dropdown ", {
+	List = {"Value1", "Value2", "Value3", "Value4"},
+	Default = "None"}, function(value)
+	print(value)
+end)
+
+Section1:CreateButton("Refresh Example", function ()
+	local newlist = {"resf", "uwua", "fsk"}
+	dropdown:Clear()
+	wait(1)
+	dropdown:Add(newlist)
 end)
 
 if getgenv().Tvk then 
